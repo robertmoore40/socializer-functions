f1c2a489de0eaff143eefb5b7da6fe9dd21e768a
robertmoore40 <robertcmoore92@gmail.com>
2020-11-03 19:29:34 -0500

    added logic errors

--- functions/util/fbAuth.js
+++ functions/util/fbAuth.js
@@ -5,3 +5,8 @@ module.exports = (req, res, next) => {
     if (  req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer ')
       ) 
+      idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+      console.error('No token found');
+      return res.status(403).json({ error: 'Unauthorized' });
+    }
